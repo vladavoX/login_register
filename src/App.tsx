34152b9bf5c3f8e232e20438fbbdc49f8efe8367
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-02-06 12:41:37 +0100

    Header of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 function App() {
   return (
-    <div>
-      <h1 className='text-green-900'>Hello World</h1>
+    <div className='flex items-center justify-center h-screen bg-gray-800'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-4xl text-gray-50'>Sing in</h1>
+        <div className='flex flex-col items-center justify-center'>
+          <p className='text-xl text-gray-200'>
+            Sing in and nothing will happen, becuase that is all this app does!
+          </p>
+          <p className='text-gray-400'>
+            Okay maybe it will show you a little message of your user data, but
+            that's it. Nothing else.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
